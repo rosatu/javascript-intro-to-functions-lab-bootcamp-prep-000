1e6178c4ecfbb8264de85ce0ca86c0fbc1ee1e97
Rosa Tu <rosalina.tu@gmail.com>
2018-05-22 16:03:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,32 +15,14 @@ function logWhisper(string) {
 var uppercase = "HELLO!"
 var lowercase = 'hello!'
 var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase //
 
 function sayHiToGrandma(string)
 {
-  if (lowercase.toLowerCase() === lowercase)
+  if (string===lowercase.toLowerCase() === lowercase)
   return '${"I can\'t hear you!"}'
-  else if (uppercase.toUpperCase() === uppercase)
+  else if (string===uppercase.toUpperCase() === uppercase)
   return '${"YES INDEED!"}'
   else if (string===('I love you, Grandma'))
   return `${"I love you, too."}` 
+  else if (string===mixedCase.toLowerCase() === mixedCase)
 }
-
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
