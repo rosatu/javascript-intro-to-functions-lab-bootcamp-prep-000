057bf67bed99c654ee75bf01381b46522d39718a
Rosa Tu <rosalina.tu@gmail.com>
2018-05-22 15:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase //
 function sayHiToGrandma(string)
 {
   if (string===string.toLowerCase)
